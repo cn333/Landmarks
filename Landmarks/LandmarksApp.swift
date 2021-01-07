8b7fd8ea9fb06c81d16909a18247158c1ab2866c
Jack <wa@student.tu.ac.th>
2021-01-07 11:10:38 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ECE TU on 7/1/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

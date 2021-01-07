b253c86cf510c08d673d15415667f0f064cf9e85
Jack <wa@student.tu.ac.th>
2021-01-07 15:55:02 +0700

    Handling User Input

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by ECE TU on 7/1/2564 BE.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }, label: {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? .yellow : .gray)
+        })
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
